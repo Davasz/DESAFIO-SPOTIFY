0cbe3c92781f3cc412d32e0e8681423efc77c2a2
Davasz <davidavi200615@gmail.com>
2024-02-27 16:10:13 -0300

    🧱Feat: Home page view

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <main>
+        <div class="salutation">
+            <h1>
+                <span>Olá</span>, seja bem-vindo!
+            </h1>
+        </div>
+
+        <TheSearch></TheSearch>
+    </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+// Imports
+import TheSearch from '@/components/TheSearch.vue';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    components: {
+        TheSearch
+    }
 }
+
 </script>
+
+<style scoped>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        color: aliceblue;
+    }
+    .salutation {
+        font-family: 'Sansation', sans-serif;
+        font-size: 1em;
+    }
+    .salutation span {
+        color: #B1B1B1;
+    }
+</style>
