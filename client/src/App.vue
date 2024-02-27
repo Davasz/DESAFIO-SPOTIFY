8d2b5aeaf85fe1e8821c32ebd59076db53b043f7
Davasz <davidavi200615@gmail.com>
2024-02-27 16:07:16 -0300

    ✨Feat: Initial configs

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+
+  <TheHeader />
+  <router-view />
+
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+// Import global css
+import '@/assets/css/global.css'
+// Import components
+import TheHeader from '@/components/TheHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader
+  },
+  setup() {
+
+  }
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
