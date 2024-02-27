24ac424262f5397c27fdea92a7d2a4be77948594
Davasz <davidavi200615@gmail.com>
2024-02-27 16:09:46 -0300

    🧱Feat: Search input component

---
+++ client/src/components/TheSearch.vue
@@ -0,0 +1,16 @@
+<template>
+    <input class="search" type="text">
+</template>
+
+<style scoped>
+.search {
+    font-family: sans-serif;
+    width: 90vw;
+    height: 3.5rem;
+    background-color: #35294D;
+    color: #B3B1B1;
+    font-size: 1.5em;
+}
+
+
+</style>
