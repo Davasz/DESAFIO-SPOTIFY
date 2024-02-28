1d73e8cb947353291d6cf4b20453067e2b45c95f
Davasz <davidavi200615@gmail.com>
2024-02-28 10:11:49 -0300

    ✨Feat: Add Card artist component and methods

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -6,35 +6,75 @@
             </h1>
         </div>
 
+        <!-- Search component -->
         <TheSearch></TheSearch>
+
+        <!-- Card component -->
+        <!-- Take each artist from the state store and generate a card with information about each one -->
+        <CardsArtists v-for="(artist, index) in store.state.artist" :key="index" :imgArtist="artist.images[0]">
+            
+            <!-- Transmitting name information via slots -->
+            <template v-slot:name>
+                <h1 class="card-name">{{ artist.name }}</h1>
+            </template>
+
+            <!-- Transmitting genres information via slots -->
+            <template v-slot:genres>
+                <p class="card-gender">{{ artist.genres[0] }}</p>
+            </template>
+
+        </CardsArtists>
+
+
     </main>
 </template>
 
 <script>
 
-// Imports
+// Import components
 import TheSearch from '@/components/TheSearch.vue';
+import CardsArtists from '@/components/CardsArtists.vue';
+// Import vue functions
+
+// Import store
+import { useStore } from 'vuex';
 
 export default {
     components: {
-        TheSearch
+        TheSearch,
+        CardsArtists
+    },
+
+    setup() {
+        // Store initialization
+        const store = useStore();
+
+        // Returning the variables
+        return {
+            store
+        }
     }
 }
 
 </script>
 
 <style scoped>
-    main {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        color: aliceblue;
-    }
-    .salutation {
-        font-family: 'Sansation', sans-serif;
-        font-size: 1em;
-    }
-    .salutation span {
-        color: #B1B1B1;
-    }
+
+/* Mobile First */
+main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: aliceblue;
+}
+
+.salutation {
+    font-family: 'Sansation', sans-serif;
+    font-size: 1em;
+    margin-bottom: 2rem;
+}
+
+.salutation span {
+    color: #B1B1B1;
+}
 </style>
