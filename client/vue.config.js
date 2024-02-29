d22ce9076e9c7644ebf0b16ffec63ff9a5cafb03
Davasz <davidavi200615@gmail.com>
2024-02-29 07:45:50 -0300

    🐛Fix: Fix dotenv bug

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        "fs": false,
+        "os": false,
+        "path": false
+      }
+    }
+  }
+});
