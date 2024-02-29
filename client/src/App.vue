e3dd747c8dfe9c8c948f3f820c634796c7db54a5
Davasz <davidavi200615@gmail.com>
2024-02-29 19:50:27 -0300

    ✨ Feat: Footer component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,27 @@
 <template>
 
-  <TheHeader />
+  <TheHeader />  <!-- Dynamic header -->
+
   <router-view />
+  
+  <TheFooter /> <!-- Dynamic footer -->
 
 </template>
 
 <script>
+
 // Import global css
 import '@/assets/css/global.css'
+
 // Import components
 import TheHeader from '@/components/TheHeader.vue'
+import TheFooter from '@/components/TheFooter.vue';
 
 export default {
   name: 'App',
   components: {
-    TheHeader
+    TheHeader,
+    TheFooter
   },
 }
 
---
+++ client/src/components/TheFooter.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <footer>
+        <h1>Feito por <a href="https://www.linkedin.com/in/davi-souza-745155246/">Davi Souza</a></h1>
+    </footer>
+    
+</template>
+
+<style scoped>
+footer {
+    margin-top: 2rem;
+    font-family: 'Sansation';
+    font-size: 0.7em;
+    color: #FFFFFF;
+    text-align: center;
+    padding: 2rem;
+    bottom: 0;
+}
+</style>
